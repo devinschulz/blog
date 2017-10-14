7f1a9bec8d2b0483567b943da27cd1f71a78f627
Devin Schulz <devin@devinschulz.com>
2017-10-14 10:41:43 -0400

    Add lost css and update supported browser list

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -2,16 +2,20 @@ const atImport = require('postcss-import')
 const cssnano = require('cssnano')
 const cssnext = require('postcss-cssnext')
 const gulp = require('gulp')
+const lost = require('lost')
+const plumber = require('gulp-plumber')
 const postcss = require('gulp-postcss')
 const precss = require('precss')
-const plumber = require('gulp-plumber')
 
 const { isProduction } = require('./env')
 
 const plugins = [
   atImport(),
+  lost(),
   precss(),
-  cssnext()
+  cssnext({
+    browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1, not ie <= 10'
+  })
 ]
 
 if (isProduction) {
