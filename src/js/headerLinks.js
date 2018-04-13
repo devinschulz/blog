b44989c66dbc7f559e038660cd93f05f79140c53
Devin Schulz <devin@devinschulz.com>
2018-04-13 16:09:13 -0400

    Update anchor/external links to use reference sprite

--- src/js/headerLinks.js
+++ src/js/headerLinks.js
@@ -4,7 +4,7 @@ const createAnchorForID = id => {
   anchor.className = 'c-article__link'
   anchor.href = `#${id}`
   anchor.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="c-article__icon"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>'
+    '<svg class="c-icon c-icon--link"><use xlink:href="#shape-link"></use></svg>'
   return anchor
 }
 
