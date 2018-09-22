f462339424ce2e20c8a429a31627ff0b8975f3cb
Devin Schulz <devin@devinschulz.com>
2018-09-21 21:07:31 -0400

    Revamp for layout v2

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -1,13 +1,57 @@
 const gulp = require('gulp')
 const purgecss = require('gulp-purgecss')
+const path = require('path')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const eyeglass = require('eyeglass')
+const tailwind = require('tailwindcss')
+
+const { isProduction } = require('./env')
+
+const plugins = [
+  require('postcss-import')(),
+  require('postcss-mixins')(),
+  require('postcss-nested')(),
+  require('tailwindcss')(path.join(__dirname, '../tailwind.config.js')),
+  require('precss')(),
+  require('css-mqpacker')({
+    sort: true,
+  }),
+  require('autoprefixer')({
+    browsers: ['last 2 versions', '> 1%'],
+  }),
+  require('postcss-preset-env')(),
+]
+
+if (isProduction) {
+  plugins.push(
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    })
+  )
+}
 
 gulp.task('css', () =>
+  gulp
+    .src('assets/styles/main.css')
+    .pipe(plumber())
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('assets'))
+)
+
+gulp.task('css:post', () =>
   gulp
     .src('public/**/*.css')
     .pipe(
       purgecss({
         content: ['public/**/*.html'],
-        whitelist: ['fonts-loaded', 'theme-dark'],
       })
     )
     .pipe(gulp.dest('public/'))
--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,3 +1,3 @@
 const gulp = require('gulp')
 
-gulp.task('default', ['scss', 'scripts'])
+gulp.task('default', ['css', 'scripts'])
