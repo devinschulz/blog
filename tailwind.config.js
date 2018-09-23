b717dcd918bd730e13744c7dd35b9002fa51eef1
Devin Schulz <devin@devinschulz.com>
2018-09-22 20:46:25 -0400

    Spacing refinements and colour updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -98,7 +98,7 @@ let colors = {
   'blue-darkest': '#12283a',
   'blue-darker': '#1c3d5a',
   'blue-dark': '#2779bd',
-  blue: '#3490dc',
+  blue: '#0070c9',
   'blue-light': '#6cb2eb',
   'blue-lighter': '#bcdefa',
   'blue-lightest': '#eff8ff',
