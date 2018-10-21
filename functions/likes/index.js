af2731c8897f9aed12ed232a0f3991422638b63e
Devin Schulz <devin@devinschulz.com>
2018-10-21 14:26:38 -0400

    Switch from firebase to firestore

--- functions/likes/index.js
+++ functions/likes/index.js
@@ -14,44 +14,56 @@ app.use(
 
 admin.initializeApp(functions.config().firebase)
 
-function handleGET(req, res) {
-  return admin
-    .database()
-    .ref(`/likes/${req.params.id}`)
-    .once('value', snapshot => {
-      const value = snapshot.val() || { count: 0 }
-      res.status(200).json(value)
-    })
-}
+const db = admin.firestore()
+const likes = db.collection('likes')
 
-function handlePUT(req, res) {
-  return admin
-    .database()
-    .ref(`/likes/${req.params.id}`)
-    .transaction(
-      currentData =>
-        currentData ? { count: currentData.count + 1 } : { count: 1 },
-      (error, committed, snapshot) => {
-        if (error) {
-          return res.status(500).json({ error })
-        }
-        if (!committed) {
-          return res.status(500).json({ error: 'Transaction aborted' })
-        }
-        res.status(200).json(snapshot.val())
+const get = (req, res) =>
+  likes
+    .doc(req.params.id)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        res.status(200).json({ count: 0 })
+      } else {
+        res.status(200).send(doc.data())
       }
+    })
+
+const put = (req, res) =>
+  db
+    .runTransaction(t =>
+      t.get(likes.doc(req.params.id)).then(doc => {
+        const count = doc.exists ? doc.data().count + 1 : 1
+        const method = doc.exists ? 'update' : 'set'
+        t[method](likes.doc(req.params.id), { count })
+        return Promise.resolve(count)
+      })
     )
-}
+    .then(count => res.status(200).json({ count }))
+    .catch(error =>
+      res
+        .status(500)
+        .json({ status: 500, message: 'Failed to update count', error })
+    )
 
-function handleGETAll(req, res) {
-  return admin
-    .database()
-    .ref('/likes')
-    .once('value', snapshot => res.status(200).json(snapshot.val()))
-}
+const all = (req, res) =>
+  likes
+    .get()
+    .then(snapshots => {
+      const response = {}
+      snapshots.forEach(doc => {
+        response[doc.id] = doc.data()
+      })
+      res.status(200).json(response)
+    })
+    .catch(error =>
+      res
+        .status(500)
+        .json({ status: 500, message: 'Failed to fetch all likes' })
+    )
 
-app.get('/:id', handleGET)
-app.put('/:id', handlePUT)
-app.get('/', handleGETAll)
+app.get('/:id', get)
+app.put('/:id', put)
+app.get('/', all)
 
 exports.likes = functions.https.onRequest(app)
