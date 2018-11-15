ce4539513894e525bbbd63d67aaf53f0c96910e0
Devin Schulz <devin@devinschulz.com>
2018-11-14 22:29:17 -0500

    Restructure functions to work with firebase deploy

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const { likes } = require('./likes')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.likes = likes
