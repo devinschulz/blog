2b6e63a5bafb23d428ffcc59a305c3e9a78467b3
Devin Schulz <devin@devinschulz.com>
2019-06-07 16:21:45 -0400

    Add project image spinner

--- src/js/lazy_images_controller.js
+++ src/js/lazy_images_controller.js
@@ -3,7 +3,26 @@ import LazyLoad from 'vanilla-lazyload'
 
 export default class extends Controller {
   initialize() {
-    this.lazy = new LazyLoad()
+    this.lazy = new LazyLoad({
+      callback_reveal(element) {
+        // Code smell, refactor
+        if (element.classList.contains('c-browser__image')) {
+          const vp = element.closest('.c-browser__viewport')
+          if (vp) {
+            vp.classList.add('loading')
+          }
+        }
+      },
+      callback_loaded(element) {
+        // Code smell, refactor
+        if (element.classList.contains('c-browser__image')) {
+          const vp = element.closest('.c-browser__viewport')
+          if (vp) {
+            vp.classList.remove('loading')
+          }
+        }
+      },
+    })
   }
 
   connect() {
