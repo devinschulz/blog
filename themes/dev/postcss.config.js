edb4e310377ed1a75c73669924666132c2cd33b2
Devin Schulz <devin@devinschulz.com>
2020-02-21 22:25:40 -0500

    Add prettier, tailwind and postcss conafig

--- themes/dev/postcss.config.js
+++ themes/dev/postcss.config.js
@@ -1,18 +1,24 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
+const path = require('path')
 
 module.exports = {
   plugins: [
+    require('postcss-nested'),
     require('postcss-import'),
-    require('tailwindcss'),
+    require('tailwindcss')(path.join(__dirname, './tailwind.config.js')),
     require('postcss-preset-env'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          require('@fullhuman/postcss-purgecss')({
+            // Specify the paths to all of the template files in your project
+            content: ['./src/**/*.html'],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }),
+          require('cssnano')({
+            preset: ['default', { discardComments: { removeAll: true } }],
+          }),
+        ]
+      : []),
   ],
 }
--- themes/dev/tailwind.config.js
+++ themes/dev/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      alt: ['Inter var', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {},
