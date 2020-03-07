131447fc375d6010c9d30c1dc337eb11bc16cb05
Devin Schulz <devin@devinschulz.com>
2020-03-06 20:26:46 -0500

    Narrow down colors, again

--- themes/dev/tailwind.config.js
+++ themes/dev/tailwind.config.js
@@ -9,19 +9,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        blue: {
-          100: 'hsl(234, 30%, 54%)',
-          200: 'hsl(234, 40%, 54%)',
-          300: 'hsl(234, 50%, 54%)',
-          400: 'hsl(234, 60%, 54%)',
-          500: 'hsl(234, 70%, 54%)',
-          600: 'hsl(234, 80%, 54%)',
-          700: 'hsl(234, 90%, 54%)',
-          800: 'hsl(234, 100%, 54%)',
-        },
-        orange: {
-          500: 'var(--colors-primary)',
-        },
+        primary: 'var(--colors-primary)',
+        secondary: 'var(--colors-secondary)',
         gray: {
           '100': '#f5f5f5',
           '200': '#eeeeee',
