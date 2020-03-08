1574aa86316a3cb0be81ce6b425cc1cc24aef27a
Devin Schulz <devin@devinschulz.com>
2020-03-07 21:56:42 -0500

    Add related posts

---
+++ assets/js/related.js
@@ -0,0 +1,10 @@
+;(function() {
+  const viewAllButton = document.querySelector('.js-view-all')
+  if (viewAllButton) {
+    viewAllButton.addEventListener('click', event => {
+      event.preventDefault()
+      document.querySelector('.js-all').classList.remove('hidden')
+      viewAllButton.parentNode.removeChild(viewAllButton)
+    })
+  }
+})()
