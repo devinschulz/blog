e6e55a9235cb296ae83d70bef38a8ead69ecd2bf
Devin Schulz <devin@devinschulz.com>
2020-03-12 22:32:56 -0400

    Add PostCSS mixins

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,9 @@ const path = require('path')
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-nested'),
+    require('postcss-simple-vars'),
     require('tailwindcss'),
     require('postcss-preset-env'),
     ...(process.env.NODE_ENV === 'production'
