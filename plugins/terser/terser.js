68e027078dd850259e23c8127e4e2f7a4282fa24
Devin Schulz <devin@devinschulz.com>
2020-04-13 22:29:01 -0400

    Create terser plugin for minification

---
+++ plugins/terser/terser.js
@@ -0,0 +1,45 @@
+const terser = require('terser')
+const path = require('path')
+const fs = require('fs')
+
+function minifyFile(pluginConfig) {
+  return (file) => {
+    const contents = fs.readFileSync(file, 'utf8')
+    const result = terser.minify(contents, pluginConfig.options)
+    fs.writeFileSync(file, result.code, 'utf8')
+
+    if (pluginConfig.debugMode) {
+      console.log('Minified', file)
+    }
+  }
+}
+
+function isJavaScript(file) {
+  return path.extname(file) === '.js'
+}
+
+function withPath(pluginConfig) {
+  return (file) => path.join(__dirname, '../../', pluginConfig.src, file)
+}
+
+module.exports = function () {
+  return {
+    name: 'terser',
+    onBuild({ pluginConfig, utils }) {
+      if (!pluginConfig.src) {
+        return utils.build.failPlugin('No pluginConfig.src found')
+      }
+      if (!pluginConfig.options) {
+        pluginConfig.options = {}
+      }
+      try {
+        fs.readdirSync(path.join(__dirname, '../../', pluginConfig.src))
+          .filter(isJavaScript)
+          .map(withPath(pluginConfig))
+          .forEach(minifyFile(pluginConfig))
+      } catch (error) {
+        utils.build.failPlugin('Failed to minify assets with terser', error)
+      }
+    },
+  }
+}
