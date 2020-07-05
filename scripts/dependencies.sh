da6df6daa21f8ab3bb1ffb7a91405bd6f39523ec
Devin Schulz <devin@devinschulz.com>
2020-07-04 22:35:32 -0400

    Replace swup with instant.page

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,9 +3,6 @@
 rm -rf assets/dependencies
 mkdir -p assets/dependencies
 
-cp node_modules/swup/dist/swup.js assets/dependencies/swup.js
-cp node_modules/@swup/preload-plugin/dist/SwupPreloadPlugin.js assets/dependencies/SwupPreloadPlugin.js
-cp node_modules/@swup/head-plugin/dist/SwupHeadPlugin.js assets/dependencies/SwupHeadPlugin.js
-cp node_modules/@swup/scroll-plugin/dist/SwupScrollPlugin.js assets/dependencies/SwupScrollPlugin.js
+cp node_modules/instant.page/instantpage.js assets/dependencies/instantpage.js
 
 echo "Dependencies copied!"
