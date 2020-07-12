9971ab2a5f9983c07f7b67938efe16ae089ec8d8
Devin Schulz <devin@devinschulz.com>
2020-07-12 16:09:20 -0400

    Revert "Update tag styles"
    This reverts commit dfb5a64d568e1aec8341d1274df7ed2c350b08d5.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-color-function'),
     require('postcss-mixins'),
     require('postcss-nested'),
     require('postcss-simple-vars'),
