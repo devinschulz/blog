dfb5a64d568e1aec8341d1274df7ed2c350b08d5
Devin Schulz <devin@devinschulz.com>
2020-07-12 16:05:12 -0400

    Update tag styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-color-function'),
     require('postcss-mixins'),
     require('postcss-nested'),
     require('postcss-simple-vars'),
