609eba5b48aff21fafa8f6d6aeb1d8d529188a27
Devin Schulz <devin@devinschulz.com>
2023-11-09 20:59:36 -0500

    Fix postcss imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,21 @@
+/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-mixins'),
-    require('postcss-nested'),
-    require('postcss-color-function'),
-    require('postcss-simple-vars'),
-    require('tailwindcss'),
-    require('postcss-preset-env'),
-    ...(process.env.NODE_ENV === 'production'
+    require("postcss-import")({
+      path: ["assets/css"],
+    }),
+    require("postcss-mixins"),
+    require("postcss-nested"),
+    require("postcss-color-function"),
+    require("postcss-simple-vars"),
+    require("tailwindcss"),
+    require("postcss-preset-env"),
+    ...(process.env.NODE_ENV === "production"
       ? [
-          require('cssnano')({
-            preset: ['default', {discardComments: {removeAll: true}}],
+          require("cssnano")({
+            preset: ["default", { discardComments: { removeAll: true } }],
           }),
         ]
       : []),
   ],
-}
+};
