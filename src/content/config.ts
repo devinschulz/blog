7f2a2db28cfeaa8e3b1efd79124d24819ca69094
Devin Schulz <devin@devinschulz.com>
2024-02-02 22:10:12 -0500

    Additional blog config

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    canonical: z.string().optional(),
   }),
 });
 
