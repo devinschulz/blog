f3de8307c212de92a61139f6e46ad126364eadea
Devin Schulz <devin@devinschulz.com>
2024-02-13 11:43:12 -0500

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://devinschulz.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
 });
